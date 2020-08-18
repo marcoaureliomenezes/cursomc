3dd7fea305f965cdcf3171592971565b34b49422
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-08-18 13:37:08 -0300

    Endpoint clientes/id finished.

--- src/main/java/com/marcomenezes/cursomc/domain/Endereco.java
+++ src/main/java/com/marcomenezes/cursomc/domain/Endereco.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Endereco implements Serializable{
 
@@ -23,6 +25,7 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private String cep;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="cliente.id")
 	private Cliente cliente;
@@ -35,7 +38,6 @@ public class Endereco implements Serializable{
 	public Endereco() {
 		
 	}
-
 	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep,
 			Cliente cliente, Cidade cidade) {
 		super();
