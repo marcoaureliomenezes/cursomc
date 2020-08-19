fa79fef6afc82d6509514d0c86568a6b1123a0fc
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-08-19 15:37:37 -0300

    Updating project for the second chapter

--- src/main/java/com/marcomenezes/cursomc/domain/Pagamento.java
+++ src/main/java/com/marcomenezes/cursomc/domain/Pagamento.java
@@ -79,7 +79,6 @@ public abstract class Pagamento implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-	
 
 	@Override
 	public boolean equals(Object obj) {
