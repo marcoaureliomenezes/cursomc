b0404ea4b861ac3b92075f32a50790b1234d4493
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-08-19 14:22:15 -0300

    Update using @JsonIgnore instead of @JsonManagedReference and @JsonBackReference.

--- src/main/java/com/marcomenezes/cursomc/domain/Cidade.java
+++ src/main/java/com/marcomenezes/cursomc/domain/Cidade.java
@@ -22,7 +22,6 @@ public class Cidade implements Serializable {
 	private Integer Id;
 	private String Nome;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
