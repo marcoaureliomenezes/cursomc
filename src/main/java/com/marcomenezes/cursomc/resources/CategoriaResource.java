07030f8d30ec01b1734584517da3fdc27d113d0a
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-08-20 00:59:32 -0300

    Listing all categories without the corresponding products.

--- src/main/java/com/marcomenezes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcomenezes/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.marcomenezes.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marcomenezes.cursomc.domain.Categoria;
+import com.marcomenezes.cursomc.dto.CategoriaDTO;
 import com.marcomenezes.cursomc.services.CategoriaService;
 
 @RestController
@@ -27,6 +30,12 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listaDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDto);
+	}
 	
 	@RequestMapping(method=RequestMethod.POST, consumes = "application/json")
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
