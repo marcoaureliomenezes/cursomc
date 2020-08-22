8a5145262561267b6c0db86799d11fc400feb3d2
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-08-22 19:54:35 -0300

    Validation for email in update client method.

--- src/main/java/com/marcomenezes/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/marcomenezes/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.marcomenezes.cursomc.domain.Cliente;
+import com.marcomenezes.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
