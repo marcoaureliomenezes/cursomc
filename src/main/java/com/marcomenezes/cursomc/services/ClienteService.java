b4dc5f8f94fc568e6f253110912e6694e2579891
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-09-18 11:24:51 -0300

    getByEmail method implemented

--- src/main/java/com/marcomenezes/cursomc/services/ClienteService.java
+++ src/main/java/com/marcomenezes/cursomc/services/ClienteService.java
@@ -56,6 +56,20 @@ public class ClienteService {
 		return repo.findAll();
 	}
 	
+	
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Denied access");
+		}
+		Cliente obj = repo.findByEmail(email);
+		if(obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado. Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+	
 //	Insert a client method
 	@Transactional
 	public Cliente insert(Cliente obj) {
