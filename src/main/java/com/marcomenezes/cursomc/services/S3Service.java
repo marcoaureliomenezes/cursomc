f948be8d8fbcf63b451deb840e73b5b072117399
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-09-22 11:46:22 -0300

    Handling Amazon Exceptions

--- src/main/java/com/marcomenezes/cursomc/services/S3Service.java
+++ src/main/java/com/marcomenezes/cursomc/services/S3Service.java
@@ -2,6 +2,8 @@ package com.marcomenezes.cursomc.services;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import java.net.URI;
-import java.net.URISyntaxException;
+import com.marcomenezes.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -35,7 +36,7 @@ public class S3Service {
 			return uploadFile(is, filename, contentType);
 		} 
 		catch (IOException e) {
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 
 	}
@@ -51,7 +52,7 @@ public class S3Service {
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} 
 		catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 }
